a0b2a9a4a0eb9cc811824985db305ff012a793ff
Justin Hawkins <justin@hawkins.id.au>
2022-01-16 23:00:29 +1030

    Simple versioning for releases

---
+++ build_release.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+open my $fh, "<", "main.go" || die $!;
+
+my $version;
+while (<$fh>) {
+  # CurrentVersion: "v0.04"
+  $version = $1 if /CurrentVersion\s*=\s*"(v[\d\.]+)"/;
+}
+close $fh;
+
+die "no version?" unless defined $version;
+
+# quit if tests fail
+system("go test ./...") && die "not building release with failing tests";
+
+# so lazy
+system "rm", "-rf", "release", "dist";
+system "mkdir", "release";
+system "mkdir", "dist";
+
+my %build = (
+  win   => { env => { GOOS => 'windows', GOARCH => 'amd64' }, filename => 'netgiv.exe' },
+  linux => { env => { GOOS => 'linux',   GOARCH => 'amd64' }, filename => 'netgiv' },
+  mac   => { env => { GOOS => 'darwin',  GOARCH => 'amd64' }, filename => 'netgiv' },
+); 
+
+foreach my $type (keys %build) {
+  mkdir "release/$type";
+}
+
+foreach my $type (keys %build) {
+  local $ENV{GOOS}   = $build{$type}->{env}->{GOOS};
+  local $ENV{GOARCH} = $build{$type}->{env}->{GOARCH};
+  system "go", "build", "-o", "release/$type/" . $build{$type}->{filename};
+  system "zip", "-j", "dist/netgiv-$type-$version.zip", ( glob "release/$type/*" );
+}
--- main.go
+++ main.go
@@ -11,6 +11,8 @@ import (
 	"github.com/spf13/viper"
 )
 
+var CurrentVersion = "v0.0.1"
+
 func main() {
 	isServer := flag.Bool("server", false, "Run netgiv in server mode")
 
